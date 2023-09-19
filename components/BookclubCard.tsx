8467f754b66cfd35412d55cdce5bca29a68e9294
Rich Mcveigh <richmcveigh1@gmail.com>
2023-09-19 14:33:06 +0100

    adds a style sheet git push origin addstylesheet

--- components/BookclubCard.tsx
+++ components/BookclubCard.tsx
@@ -1,15 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { styles } from '../stylesheet'
 
 const BookclubCard: React.FC<{bookclubName: string}> = ({bookclubName}) => {
   return (
-    <View style={{
-        borderWidth: 5,
-        borderBlockColor: 'blue',
-        flex: 0.2,
-        alignItems: 'center', 
-        justifyContent: 'center'
-    }}>
+    <View style={styles.book}>
       <Text>{bookclubName}</Text>
     </View>
   )
