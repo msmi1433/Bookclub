d16a81e78b20edfdd1228dd8698496466cb8f19a
taryncodes@github.com <taryn.bilsborough@gmail.com>
2023-09-20 14:15:24 +0100

    added user avatar image

--- components/profilePage/username-card.tsx
+++ components/profilePage/username-card.tsx
@@ -1,16 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 
-const Username: React.FC<{user:{user_username: string}}> = ({user})=> {
+const Username: React.FC<{user:{user_username: string, user_avatar_img: string}}> = ({user})=> {
   return (
     <View style={{
+
         borderWidth: 5,
         borderBlockColor: 'blue',
         flex: 0.2,
         alignItems: 'center', 
-        justifyContent: 'center'
+        justifyContent: "space-evenly",
+        padding:5
+
+
     }}>
+      <Image style={{ width: 100, height: 100}} source={{uri: user.user_avatar_img}}/> 
       <Text>{user.user_username}</Text>
+     
     </View>
   )
 }
