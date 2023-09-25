6b689727dec109fbbbd3e58967e1b7c6a3dd0d9c
Myles Smith <mylessmith@Myless-MacBook-Pro.local>
2023-09-25 10:15:20 +0100

    fixes merges

--- addingData.tsx
+++ addingData.tsx
@@ -16,10 +16,6 @@ export const addUser = (uid: string, username: string) => {
     user_bookclubs: [],
     user_fave_books: [],
   }).catch((err) => alert(err));
-
-export const addUser = (uid: string, username: string) => {
-  return setDoc(doc(db,'users',uid),{user_username: username, user_avatar_image:"",user_bio:'',user_bookclubs:[],user_fave_books:[]})
-  .catch((err)=>alert(err))
 };
 
 export const createBookClub = (clubData: {
