821b207536abe71dc19d79fbf61b627ad157219b
Myles Smith <mylessmith@Myless-MacBook-Pro.local>
2023-09-28 12:30:56 +0100

    fixes first read

--- screens/FirstBook.tsx
+++ screens/FirstBook.tsx
@@ -1,46 +1,65 @@
-import { View, Text, Image } from 'react-native'
-import BookSearch from '../components/BookSearch';
-import React from 'react'
-import { Route } from '@react-navigation/native';
-import { useState } from 'react';
-import { setFirstRead } from '../addingData';
+import { View, Text, Image, ScrollView } from "react-native";
+import BookSearch from "../components/BookSearch";
+import React from "react";
+import { Route } from "@react-navigation/native";
+import { useState } from "react";
+import { setFirstRead } from "../addingData";
+import { styles } from "../stylesheet";
 
 type RouteParams = {
-    route: {
-      params: {
-        bookclub_id: string;
-      };
+  route: {
+    params: {
+      bookclub_id: string;
     };
   };
+};
 
 type CurrentRead = {
-    author: string;
-    book_name: string;
-    description: string;
-    img_url: string
-}
+  author: string;
+  book_name: string;
+  description: string;
+  img_url: string;
+};
 
-  
-export const FirstBook: React.FC<RouteParams> = ({route}: RouteParams) => {
-    const { bookclub_id } = route.params;
-    const [currentRead, setCurrentRead] = useState<CurrentRead | null>(null)
-    return (
-        <View>
-          <Text>NextBook</Text>
-          <BookSearch
-            callbackFn={setFirstRead}
-            stateSetter={setCurrentRead}
-            bookclub_id={bookclub_id}
-          />
-          <Text>To change the first read, please use the search bar above.</Text>
-          <Text>Our first read is...</Text>
-          <Image
-            source={{ uri: currentRead?.img_url}}
-            style={{ width: 100, height: 150 }}
-          />
-          <Text>{currentRead?.book_name}</Text>
-          <Text>{currentRead?.author}</Text>
-          <Text>{currentRead?.description}</Text>
-        </View>
-      );
-    };
+export const FirstBook: React.FC<RouteParams> = ({ route }: RouteParams) => {
+  const { bookclub_id } = route.params;
+  const [currentRead, setCurrentRead] = useState<CurrentRead | null>(null);
+  return (
+    <ScrollView>
+      <View>
+        <BookSearch
+          callbackFn={setFirstRead}
+          stateSetter={setCurrentRead}
+          bookclub_id={bookclub_id}
+        />
+        <Text style={styles.header}>Our first read is...</Text>
+        <Text style={styles.nextReadSearchText}>
+          To change the first read, please use the search bar above.
+        </Text>
+        {currentRead !== null ? (
+          <View style={styles.currentBookContainer}>
+            <View style={styles.nextBookHeader}>
+              <View style={styles.bookInfoContainer}>
+                <Image
+                  source={{ uri: currentRead?.img_url }}
+                  style={styles.bookImage}
+                />
+                <View style={styles.nextBookByHeader}>
+                  <Text style={styles.bookNameText}>
+                    {currentRead?.book_name}
+                  </Text>
+                  <Text style={styles.bookAuthorText}>
+                    by {currentRead?.author}
+                  </Text>
+                </View>
+              </View>
+            </View>
+            <Text style={styles.bookDescriptionText}>
+              {currentRead?.description}
+            </Text>
+          </View>
+        ) : null}
+      </View>
+    </ScrollView>
+  );
+};
